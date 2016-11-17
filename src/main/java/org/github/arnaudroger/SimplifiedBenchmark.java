07caee09a22e12b8759bce2fd11b030270689af8
Arnaud Roger <aroger@rapid7.com>
2016-11-17 11:55:59 +0000

    add numbers

--- src/main/java/org/github/arnaudroger/SimplifiedBenchmark.java
+++ src/main/java/org/github/arnaudroger/SimplifiedBenchmark.java
@@ -8,6 +8,30 @@ import org.openjdk.jmh.infra.Blackhole;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+
+Benchmark                                                    (nbCellsPerRow)  (nbRows)    Mode  Cnt    Score   Error  Units
+SimplifiedBenchmark.benchmarkDirect                                       10    500000  sample  242  233.642 ± 9.727  ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p0.00                 10    500000  sample       191.103          ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p0.50                 10    500000  sample       206.176          ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p0.90                 10    500000  sample       308.806          ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p0.95                 10    500000  sample       313.367          ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p0.99                 10    500000  sample       323.260          ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p0.999                10    500000  sample       325.059          ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p0.9999               10    500000  sample       325.059          ms/op
+SimplifiedBenchmark.benchmarkDirect:benchmarkDirect·p1.00                 10    500000  sample       325.059          ms/op
+SimplifiedBenchmark.benchmarkHolder                                       10    500000  sample  327  169.661 ± 5.611  ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p0.00                 10    500000  sample       143.393          ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p0.50                 10    500000  sample       157.024          ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p0.90                 10    500000  sample       235.143          ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p0.95                 10    500000  sample       240.281          ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p0.99                 10    500000  sample       255.423          ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p0.999                10    500000  sample       265.814          ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p0.9999               10    500000  sample       265.814          ms/op
+SimplifiedBenchmark.benchmarkHolder:benchmarkHolder·p1.00                 10    500000  sample       265.814          ms/op
+
+ */
+
 @State(Scope.Thread)
 public class SimplifiedBenchmark {
 
